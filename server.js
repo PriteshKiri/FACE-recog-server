38c1f685e38b8449a647cf7a3f7c8c3780e9598d
PriteshKiri <pritesh.d.kiri@gmail.com>
2022-10-07 18:24:01 +0530

    env added

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("This app is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("This app is running on port 5000");
 });
 
 // END points:
